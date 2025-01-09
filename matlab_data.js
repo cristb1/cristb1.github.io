be0a0e073b9774e16823d2ac446f2b229095f4af
bethany <bethanycrist88@gmail.com>
2025-01-09 15:26:49 -0500

    removed file

--- matlab_data.js
+++ matlab_data.js
@@ -1,9 +1,20 @@
+import Papa from 'papaparse';
+
+const fs = require('fs');
+const papa = require('papaparse');
+const file = fs.createReadStream('n_x_data.csv');
 var numColRow;
 var uData; //uData = math.matrix([[]])
 var vData;
 
-function getData(){
-    
-}
+papa.parse(file, {
+    worker: true,
+    step: function(result) {
+        numColRow = result;
+    },
+    complete: function(results, file){
+        console.log('complete');
+    }
+});
 
-getData();
+
