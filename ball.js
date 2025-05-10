e90d587607da54a3628dc02df18f61de029ddbb5
bethany <bethanycrist88@gmail.com>
2025-05-10 18:38:57 -0400

    fixed background and code indentation

--- ball.js
+++ ball.js
@@ -12,53 +12,54 @@ let ySpeedOld;
 let isPaused;
 
 function setup() {
-  createCanvas(400, 400);
-  background(100);
-  speedButton = createButton("submit");
-  inputSpeed = createInput();
-  speedButton.mousePressed(changeSpeed);
-  
-  pauseButton = createButton("pause");
-  pauseButton.mousePressed(pause)
-  
-  resumeButton = createButton("resume");
-  resumeButton.mousePressed(resume);
+    createCanvas(400, 400);
+    speedButton = createButton("submit");
+    inputSpeed = createInput();
+    speedButton.mousePressed(changeSpeed);
+    
+    pauseButton = createButton("pause");
+    pauseButton.mousePressed(pause)
+    
+    resumeButton = createButton("resume");
+    resumeButton.mousePressed(resume);
 
 }
 
 function draw() {
-  circle(x,y,rad);
-  
-  x += xSpeed;
-  y += ySpeed;
-  if(xSpeed != 0){
-    xSpeedOld = xSpeed;
-    ySpeedOld = ySpeed;
-  }
-  
-  if(x < rad || x > width - rad){
-    xSpeed = -xSpeed;
-  }
-  
-  if(y < rad || y > height - rad){
-    ySpeed = -ySpeed;
-  }
+    background(100);  
+    circle(x,y,rad);
+    
+    x += xSpeed;
+    y += ySpeed;
+    
+    if(xSpeed != 0){
+        xSpeedOld = xSpeed;
+        ySpeedOld = ySpeed;
+    }
+    
+    if(x < rad || x > width - rad){
+        xSpeed = -xSpeed;
+    }
+    
+    if(y < rad || y > height - rad){
+        ySpeed = -ySpeed;
+    }
 }
 
 function changeSpeed(){
-  xSpeed = parseInt(inputSpeed.value());
-  ySpeed = parseInt(inputSpeed.value());
-  inputSpeed.value('');
+    xSpeed = parseInt(inputSpeed.value());
+    ySpeed = parseInt(inputSpeed.value());
+    inputSpeed.value('');
 }
 
 function pause(){
-  xSpeedOld = xSpeed;
-  ySpeedOld = ySpeed;
-  xSpeed = 0;
-  ySpeed = 0;
+    xSpeedOld = xSpeed;
+    ySpeedOld = ySpeed;
+    xSpeed = 0;
+    ySpeed = 0;
 }
 
 function resume(){
-  xSpeed = xSpeedOld;
-  ySpeed = ySpeedOld;
+    xSpeed = xSpeedOld;
+    ySpeed = ySpeedOld;
 }
